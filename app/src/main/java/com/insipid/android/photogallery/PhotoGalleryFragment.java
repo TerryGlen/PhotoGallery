c24624ecfdd887274b0d64c517682802cb11a148
Terry Glinsey <saispas99@gmail.com>
2017-10-09 16:32:06 -0500

    Added parsing from Json.

--- app/src/main/java/com/insipid/android/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/insipid/android/photogallery/PhotoGalleryFragment.java
@@ -10,6 +10,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import java.io.IOException;
 
@@ -53,7 +54,9 @@ public class PhotoGalleryFragment extends Fragment {
 
         }
 
+    }
+    private class PhotoHolder extends Recyclerview.ViewHolder {
+        private TextView mTitleTextView;
 
     }
-
 }
